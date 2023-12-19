52f3b5d3faf66bc8cfac1f6f5fce9a899bf9b900
Steffi Durai Nishee. D <150005945+Steffi-nishee@users.noreply.github.com>
2023-12-20 00:23:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var counter = document.getElementById("count");
+let time = 10;
+setTimeout(() => {
+time--
+counter.innerText = time;
+
+setTimeout(() => {
+    time--
+    counter.innerText = time;
+    
+    setTimeout(() => {
+        time--
+        counter.innerText = time;
+
+        setTimeout(() => {
+            time--
+            counter.innerText = time;
+
+            setTimeout(() => {
+                time--
+                counter.innerText = time;
+
+                setTimeout(() => {
+                    time--
+                    counter.innerText = time;
+
+                    setTimeout(() => {
+                        time--
+                        counter.innerText = time;
+
+                        setTimeout(() => {
+                            time--
+                            counter.innerText = time;
+
+                            setTimeout(() => {
+                                time--
+                                counter.innerText = time;
+
+
+                                    setTimeout(() => {
+                                        time--
+                                        counter.innerText = "Happy Independence Day";
+                                        var r=document.querySelector(".red");
+                                        var q=document.querySelector(".green");
+                                        q.setAttribute("style","background-color:green");
+                                        r.setAttribute("style","background-color:red");
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
